f7cea3a1d7b8bdbfda61dda62ee580246dfd58a6
alek <wait82985@gmail.com>
2023-07-19 15:12:04 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,22 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class Solution {
     public int findMinArrowShots(int[][] points) {
-        //TODO
-        return 0;
+        if(points.length == 0){
+            return 0;
+        }
+        int arrows = 1;
+        Arrays.sort(points, Comparator.comparingInt(a -> a[1]));
+        int arrowPoint = points[0][1];
+
+        for (int i = 1; i < points.length; i++) {
+            if(points[i][0] > arrowPoint){
+                arrows++;
+                arrowPoint = points[i][1];
+            }
+        }
+
+        return arrows;
     }
 }
